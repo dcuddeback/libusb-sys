5b7cf79dff6a9984990b0448aaf5f31f741a207c
Yuriy Larin <yuriy.larin@firstbridge.io>
2020-08-27 22:24:18 +0300

    added new structs, callbacks, methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@
 
 extern crate libc;
 
-use libc::{c_void, c_int, c_uint, c_char, c_uchar, c_short, ssize_t, timeval};
+use std::convert::TryInto;
 
+use libc::{c_char, c_int, c_short, c_uchar, c_uint, c_void, ssize_t, timeval};
 
 #[repr(C)]
 pub struct libusb_context {
@@ -126,6 +127,7 @@ pub struct libusb_bos_dev_capability_descriptor {
     pub bLength: u8,
     pub bDescriptorType: u8,
     pub bDevCapabilityType: u8,
+    pub dev_capability_data: [u8;0],
 }
 
 #[allow(non_snake_case)]
@@ -135,6 +137,7 @@ pub struct libusb_bos_descriptor {
     pub bDescriptorType: u8,
     pub wTotalLength: u16,
     pub bNumDeviceCaps: u8,
+    pub dev_capability: *mut [libusb_bos_dev_capability_descriptor;0],
 }
 
 #[allow(non_snake_case)]
@@ -169,6 +172,30 @@ pub struct libusb_container_id_descriptor {
     pub ContainerId: [u8; 16],
 }
 
+#[allow(non_snake_case)]
+#[repr(C)]
+struct libusb_control_setup {
+    /// Request type. Bits 0:4 determine recipient, see
+    /// libusb_request_recipient. Bits 5:6 determine type, see
+    /// libusb_request_type. Bit 7 determines data transfer direction, see
+    /// libusb_endpoint_direction.
+    pub bmRequestType: u8,
+    /// Request. If the type bits of bmRequestType are equal to
+    /// libusb_request_type::LIBUSB_REQUEST_TYPE_STANDARD
+    /// "LIBUSB_REQUEST_TYPE_STANDARD" then this field refers to
+    /// libusb_standard_request. For other cases, use of this field is
+    /// application-specific.
+    pub bRequest: u8,
+    /// Value. Varies according to request
+    pub wValue: u16,
+    /// Index. Varies according to request, typically used to pass an index or offset
+    pub wIndex: u16,
+    /// Number of bytes to transfer
+    pub wLength: u16,
+}
+
+pub const LIBUSB_CONTROL_SETUP_SIZE: usize = std::mem::size_of::<libusb_control_setup>();
+
 #[repr(C)]
 pub struct libusb_transfer {
     pub dev_handle: *mut libusb_device_handle,
@@ -195,6 +222,7 @@ pub struct libusb_pollfd {
 pub type libusb_transfer_cb_fn = extern "C" fn(*mut libusb_transfer);
 pub type libusb_pollfd_added_cb = extern "C" fn(c_int, c_short, *mut c_void);
 pub type libusb_pollfd_removed_cb = extern "C" fn(c_int, *mut c_void);
+pub type libusb_log_cb = extern "C" fn(*mut libusb_context, c_int/*libusb_log_level*/, *const c_char /*str to log?*/);
 
 // libusb_error
 pub const LIBUSB_SUCCESS:             c_int = 0;
@@ -240,6 +268,9 @@ pub const LIBUSB_LOG_LEVEL_WARNING: c_int = 2;
 pub const LIBUSB_LOG_LEVEL_INFO:    c_int = 3;
 pub const LIBUSB_LOG_LEVEL_DEBUG:   c_int = 4;
 
+/// libusb_log_cb_mode
+pub const LIBUSB_LOG_CB_GLOBAL:  u8 = 1 << 0;
+pub const LIBUSB_LOG_CB_CONTEXT: u8 = 1 << 1;
 
 // libusb_class_code
 pub const LIBUSB_CLASS_PER_INTERFACE:       u8 = 0;
@@ -268,6 +299,18 @@ pub const LIBUSB_SPEED_LOW:     c_int = 1;
 pub const LIBUSB_SPEED_FULL:    c_int = 2;
 pub const LIBUSB_SPEED_HIGH:    c_int = 3;
 pub const LIBUSB_SPEED_SUPER:   c_int = 4;
+/// The device is operating at super speed plus (10000MBit/s).
+pub const LIBUSB_SPEED_SUPER_PLUS: c_int = 5;
+
+// libusb_supported_speed
+/// Low speed operation supported (1.5MBit/s).
+pub const LIBUSB_LOW_SPEED_OPERATION: c_int   = 1;
+/// Full speed operation supported (12MBit/s).
+pub const LIBUSB_FULL_SPEED_OPERATION: c_int  = 2;
+/// High speed operation supported (480MBit/s).
+pub const LIBUSB_HIGH_SPEED_OPERATION: c_int  = 4;
+/// Superspeed operation supported (5000MBit/s).
+pub const LIBUSB_SUPER_SPEED_OPERATION: c_int = 8;
 
 
 // libusb_descriptor_type
@@ -357,6 +400,7 @@ extern "C" {
     pub fn libusb_init(context: *mut *mut libusb_context) -> c_int;
     pub fn libusb_exit(context: *mut libusb_context);
     pub fn libusb_set_debug(context: *mut libusb_context, level: c_int);
+    pub fn libusb_set_log_cb(context: *mut libusb_context, cd: libusb_log_cb, mode: c_int /*libusb_log_level*/);
 
     pub fn libusb_get_device_list(context: *mut libusb_context, list: *mut *const *mut libusb_device) -> ssize_t;
     pub fn libusb_free_device_list(list: *const *mut libusb_device, unref_devices: c_int);
@@ -375,6 +419,7 @@ extern "C" {
     pub fn libusb_get_bus_number(dev: *const libusb_device) -> u8;
     pub fn libusb_get_port_number(dev: *mut libusb_device) -> u8;
     pub fn libusb_get_port_numbers(dev: *mut libusb_device, port_numbers: *mut u8, port_numbers_len: c_int) -> c_int;
+    pub fn libusb_get_port_path(context: *mut libusb_context, dev: *mut libusb_device, path: *mut u8, path_length: u8) -> c_int;
     pub fn libusb_get_device_address(dev: *const libusb_device) -> u8;
     pub fn libusb_get_device_speed(dev: *const libusb_device) -> c_int;
     pub fn libusb_get_max_packet_size(dev: *const libusb_device, endpoint: c_uchar) -> c_int;
@@ -446,7 +491,221 @@ extern "C" {
 
 
 // defined as static inline in libusb.h
-pub unsafe fn libusb_get_string_descriptor(dev_handle: *mut libusb_device_handle, desc_index: u8, langid: u16, data: *mut c_uchar, length: c_int) -> c_int
-{
-    libusb_control_transfer(dev_handle, LIBUSB_ENDPOINT_IN, LIBUSB_REQUEST_GET_DESCRIPTOR, (LIBUSB_DT_STRING as u16) << 8 | desc_index as u16, langid, data, length as u16, 1000)
+pub unsafe fn libusb_get_string_descriptor(dev_handle: *mut libusb_device_handle,
+                                           desc_index: u8,
+                                           langid: u16,
+                                           data: *mut c_uchar,
+                                           length: c_int) -> c_int {
+    libusb_control_transfer(dev_handle,
+                            LIBUSB_ENDPOINT_IN,
+                            LIBUSB_REQUEST_GET_DESCRIPTOR,
+                            (LIBUSB_DT_STRING as u16) << 8 | desc_index as u16,
+                            langid, data,
+                            length as u16,
+                            1000)
+}
+
+#[repr(C)]
+union ToLe16 {
+    b8: [u8;2],
+    b16: u16
+}
+
+/// Convert a 16-bit value from host-endian to little-endian format. On
+/// little endian systems, this function does nothing. On big endian systems,
+/// the bytes are swapped.
+/// x the host-endian value to convert
+/// returns the value in little-endian byte order
+pub fn libusb_cpu_to_le16(x: u16) -> u16 {
+    let _temp = ToLe16 {
+        b8: [(x >> 8) as u8,(x & 0xff) as u8]
+    };
+    unsafe { _temp.b16 }
+}
+
+/// Get the data section of a control transfer. This convenience function is here
+/// to remind you that the data does not start until 8 bytes into the actual
+/// buffer, as the setup packet comes first.
+///
+/// Calling this function only makes sense from a transfer callback function,
+/// or situations where you have already allocated a suitably sized buffer at
+/// transfer->buffer.
+///
+/// param transfer a transfer
+/// returns pointer to the first byte of the data section
+pub unsafe fn libusb_control_transfer_get_data(transfer: *mut libusb_transfer) -> *mut c_uchar {
+    (*transfer).buffer.offset(LIBUSB_CONTROL_SETUP_SIZE as isize)
+}
+
+/// Helper function to populate the setup packet (first 8 bytes of the data
+/// buffer) for a control transfer. The wIndex, wValue and wLength values should
+/// be given in host-endian byte order.
+pub unsafe fn libusb_fill_control_setup(buffer: *mut c_uchar,
+                                        bmRequestType: u8,
+                                        bRequest: u8,
+                                        wValue: u16,
+                                        wIndex: u16,
+                                        wLength: u16) {
+    let setup: &mut libusb_control_setup = std::mem::transmute(buffer);
+    setup.bmRequestType = bmRequestType;
+    setup.bRequest = bRequest;
+    setup.wValue = libusb_cpu_to_le16(wValue);
+    setup.wIndex = libusb_cpu_to_le16(wIndex);
+    setup.wLength = libusb_cpu_to_le16(wLength);
+}
+
+/// Helper function to populate the required \ref libusb_transfer fields
+/// for a control transfer.
+///
+/// If you pass a transfer buffer to this function, the first 8 bytes will
+/// be interpreted as a control setup packet, and the wLength field will be
+/// used to automatically populate the \ref libusb_transfer::length "length"
+/// field of the transfer. Therefore the recommended approach is:
+/// -# Allocate a suitably sized data buffer (including space for control setup)
+/// -# Call libusb_fill_control_setup()
+/// -# If this is a host-to-device transfer with a data stage, put the data
+///   in place after the setup packet
+/// -# Call this function
+/// -# Call libusb_submit_transfer()
+///
+/// It is also legal to pass a NULL buffer to this function, in which case this
+/// function will not attempt to populate the length field. Remember that you
+/// must then populate the buffer and length fields later.
+pub unsafe fn libusb_fill_control_transfer(transfer: *mut libusb_transfer,
+                                           dev_handle: *mut libusb_device_handle,
+                                           buffer: *mut c_uchar,
+                                           callback: libusb_transfer_cb_fn,
+                                           user_data: *mut c_void,
+                                           timeout: c_uint) {
+    // struct libusb_control_setup *setup = (struct libusb_control_setup *)(void *) buffer;
+    let setup: *mut libusb_control_setup = std::mem::transmute(buffer);
+    (*transfer).dev_handle = dev_handle;
+    (*transfer).endpoint = 0;
+    (*transfer).transfer_type = LIBUSB_TRANSFER_TYPE_CONTROL;
+    (*transfer).timeout = timeout;
+    (*transfer).buffer = buffer;
+    let ptr = setup as *const usize;
+    if !ptr.is_null()  {
+        let size_of_result = libusb_cpu_to_le16((*setup).wLength);
+        (*transfer).length = (LIBUSB_CONTROL_SETUP_SIZE + std::mem::size_of::<u16>()).try_into().unwrap();
+    }
+    (*transfer).user_data = user_data;
+    (*transfer).callback = callback;
+}
+
+/// Helper function to populate the required \ref libusb_transfer fields
+/// for a bulk transfer.
+///
+/// param transfer the transfer to populate
+/// param dev_handle handle of the device that will handle the transfer
+/// param endpoint address of the endpoint where this transfer will be sent
+/// param buffer data buffer
+/// param length length of data buffer
+/// param callback callback function to be invoked on transfer completion
+/// param user_data user data to pass to callback function
+/// param timeout timeout for the transfer in milliseconds
+pub unsafe fn libusb_fill_bulk_transfer(transfer: *mut libusb_transfer,
+                                        dev_handle: *mut libusb_device_handle,
+                                        endpoint: c_uchar,
+                                        buffer: *mut c_uchar,
+                                        length: c_int,
+                                        callback: libusb_transfer_cb_fn,
+                                        user_data: *mut c_void,
+                                        timeout: c_uint) {
+    (*transfer).dev_handle = dev_handle;
+    (*transfer).endpoint = endpoint;
+    (*transfer).transfer_type = LIBUSB_TRANSFER_TYPE_BULK;
+    (*transfer).timeout = timeout;
+    (*transfer).buffer = buffer;
+    (*transfer).length = length;
+    (*transfer).user_data = user_data;
+    (*transfer).callback = callback;
+}
+
+/// Helper function to populate the required \ref libusb_transfer fields
+/// for a bulk transfer using bulk streams.
+///
+/// Since version 1.0.19, \ref LIBUSB_API_VERSION >= 0x01000103
+///
+/// param transfer the transfer to populate
+/// param dev_handle handle of the device that will handle the transfer
+/// param endpoint address of the endpoint where this transfer will be sent
+/// param stream_id bulk stream id for this transfer
+/// param buffer data buffer
+/// param length length of data buffer
+/// param callback callback function to be invoked on transfer completion
+/// param user_data user data to pass to callback function
+/// param timeout timeout for the transfer in milliseconds
+pub unsafe fn libusb_fill_bulk_stream_transfer(transfer: *mut libusb_transfer,
+                                               dev_handle: *mut libusb_device_handle,
+                                               endpoint: c_uchar,
+                                               stream_id: u32,
+                                               buffer: *mut c_uchar,
+                                               length: c_int,
+                                               callback: libusb_transfer_cb_fn,
+                                               user_data: *mut c_void,
+                                               timeout: c_uint) {
+    libusb_fill_bulk_transfer(transfer, dev_handle, endpoint, buffer,
+                              length, callback, user_data, timeout);
+    (*transfer).transfer_type = LIBUSB_TRANSFER_TYPE_BULK_STREAM;
+    libusb_transfer_set_stream_id(transfer, stream_id);
+}
+
+/// Helper function to populate the required \ref libusb_transfer fields
+/// for an interrupt transfer.
+///
+/// param transfer the transfer to populate
+/// param dev_handle handle of the device that will handle the transfer
+/// param endpoint address of the endpoint where this transfer will be sent
+/// param buffer data buffer
+/// param length length of data buffer
+/// param callback callback function to be invoked on transfer completion
+/// param user_data user data to pass to callback function
+/// param timeout timeout for the transfer in milliseconds
+pub unsafe fn libusb_fill_interrupt_transfer(transfer: *mut libusb_transfer,
+                                             dev_handle: *mut libusb_device_handle,
+                                             endpoint: c_uchar,
+                                             buffer: *mut c_uchar,
+                                             length: c_int,
+                                             callback: libusb_transfer_cb_fn,
+                                             user_data: *mut c_void,
+                                             timeout: c_uint) {
+    (*transfer).dev_handle = dev_handle;
+    (*transfer).endpoint = endpoint;
+    (*transfer).transfer_type = LIBUSB_TRANSFER_TYPE_INTERRUPT;
+    (*transfer).timeout = timeout;
+    (*transfer).buffer = buffer;
+    (*transfer).length = length;
+    (*transfer).user_data = user_data;
+    (*transfer).callback = callback;
+}
+
+pub unsafe fn libusb_fill_iso_transfer(transfer: *mut libusb_transfer,
+                                       dev_handle: *mut libusb_device_handle,
+                                       endpoint: c_uchar,
+                                       buffer: *mut c_uchar,
+                                       length: c_int,
+                                       num_iso_packets: c_int,
+                                       callback: libusb_transfer_cb_fn,
+                                       user_data: *mut c_void,
+                                       timeout: c_uint) {
+    (*transfer).dev_handle = dev_handle;
+    (*transfer).endpoint = endpoint;
+    (*transfer).transfer_type = LIBUSB_TRANSFER_TYPE_ISOCHRONOUS;
+    (*transfer).timeout = timeout;
+    (*transfer).buffer = buffer;
+    (*transfer).length = length;
+    (*transfer).num_iso_packets = num_iso_packets;
+    (*transfer).user_data = user_data;
+    (*transfer).callback = callback;
+}
+
+pub unsafe fn libusb_set_iso_packet_lengths(transfer: *mut libusb_transfer,
+                                            length: c_uint) {
+    // int i;
+    // for (i = 0; i < (*transfer).num_iso_packets; i++) {
+    //     (*transfer).so_packet_desc[i].length = length;
+    // }
+    // for (*transfer).so_packet_desc[] =
+    unimplemented!()
 }
